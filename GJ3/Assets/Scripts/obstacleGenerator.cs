20e779e2eb7bf486c5257e5e4fa3b79aa7294cf8
AIE\s171050 <ryan0432@gmail.com>
2018-05-21 15:34:22 +1000

    Ryan working on obstacle spawners

--- GJ3/Assets/Scripts/obstacleGenerator.cs
+++ GJ3/Assets/Scripts/obstacleGenerator.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class obstacleGenerator : MonoBehaviour
 {
+    [SerializeField]
     public GameObject obtstacleTypeA;
+    [SerializeField]
     public GameObject obtstacleTypeB;
-    public List<GameObject> spownerPoints;
+    [SerializeField]
+    public GameObject ground;
+    private List< List<Vector3> > spownerSetPositions;
+
+    float floorOffset;
+    float sideOffsets;
 
     // Use this for initialization
     void Start ()
     {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
 		
 	}
+
+    void InitializeSpawnerSet()
+    {
+        Vector3 groundBound;
+        Vector3 midSpawnerPos;
+        Vector3 leftSpawnerPos;
+        Vector3 rightSpawnerPos;
+        
+        groundBound = ground.GetComponent<Renderer>().bounds.size;
+        
+        midSpawnerPos = new Vector3(groundBound.x, groundBound.y, groundBound.z + floorOffset);
+        leftSpawnerPos = midSpawnerPos + new Vector3(midSpawnerPos.x - sideOffsets, midSpawnerPos.y, midSpawnerPos.z);
+        
+        RaycastHit leftHit;
+        
+        Physics.Raycast(leftSpawnerPos, Vector3.forward, out leftHit, Mathf.Infinity);
+    }
 }
