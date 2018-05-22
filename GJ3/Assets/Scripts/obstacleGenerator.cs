a5ad49350b65b8dcac05291574a8160b12c3c2bd
RYAN-LION\RyaN <ryan0432@gmail.com>
2018-05-23 04:04:23 +1000

    Finish Procedure Obstacle Generator

--- GJ3/Assets/Scripts/obstacleGenerator.cs
+++ GJ3/Assets/Scripts/obstacleGenerator.cs
@@ -4,50 +4,60 @@ using UnityEngine;
 
 public class obstacleGenerator : MonoBehaviour
 {
-
-
     [SerializeField]
-    public GameObject ground;
+    private GameObject ground;
     [SerializeField]
-    public GameObject spawner;
-
+    private GameObject spawner;
+    private float RayDistance = 1000.0f;
     private List<List<GameObject>> spawnerSetList = new List<List<GameObject>>();
 
+    [Space(10)]
 
+    [Tooltip("Distance between floor and spawners.")]
+    [Range(0.0f, 20.0f)]
     public float floorOffset = 0.0f;
+
+    [Space(5)]
+
+    [Tooltip("Distance between spawner columns.")]
+    [Range(1.0f, 10.0f)]
     public float sideOffsets = 3.0f;
-    public float rowOffset = 10.0f;
 
-    //private float worldRotation = 0.0f;
+    [Space(5)]
 
-    public float RayDistance = 1000.0f;
+    [Tooltip("Distance between spawner rows. (by angle)")]
+    [Range(1.0f, 30.0f)]
+    public float rowOffset = 5.0f;
+
+    [Space(5)]
+
+    [Tooltip("Chance of spawning an obstacle. (min: 0 / max: 40)")]
+    [Range(0.0f, 40.0f)]
+    public float spawnChance = 20.0f;
 
     // Use this for initialization
     void Start()
     {
-        //floorOffset = 0.0f;
-        //sideOffsets = 3.0f;
-        //rowOffset = 10.0f;
-
+        spawner.transform.GetComponent<spawner>().SpawnChance = spawnChance;
         InitializeSpawnerSet();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        SpawnersSwitch();
     }
 
-    void InitializeSpawnerSet()
+    private void InitializeSpawnerSet()
     {
         for (float angle = 0; angle <= 360; angle += rowOffset)
         {
             List<GameObject> spawnerSet = new List<GameObject>();
             Vector3 rayDir = Vector3.right;
-            Quaternion rot = Quaternion.AngleAxis(angle, transform.forward);
+            Quaternion rot = Quaternion.AngleAxis(angle, ground.transform.Find("Ground").transform.forward);
             rayDir = rot * rayDir;
 
-            Vector3 rayShootPosition = (transform.position + (rayDir * RayDistance));
+            Vector3 rayShootPosition = (ground.transform.Find("Ground").transform.position + (rayDir * RayDistance));
 
             Ray r = new Ray(rayShootPosition, -rayDir);
             RaycastHit hitInfo;
@@ -55,39 +65,39 @@ public class obstacleGenerator : MonoBehaviour
             if (hitSomething)
             {
                 spawnerSet.Add(Instantiate(spawner));
-                spawnerSet[0].transform.position = hitInfo.point;
+                spawnerSet[0].transform.position = hitInfo.point;// + new Vector3 (hitInfo.normal.x + floorOffset, hitInfo.normal.y, hitInfo.normal.z);
                 spawnerSet[0].transform.up = hitInfo.normal;
-                spawnerSet[0].transform.SetParent(GameObject.Find("ObjectHolder").transform);
+                spawnerSet[0].transform.SetParent(ground.transform.Find("ObjectHolder").transform);
             }
 
-            rayShootPosition += transform.forward * sideOffsets;
+            rayShootPosition += ground.transform.Find("Ground").transform.forward * sideOffsets;
 
             r = new Ray(rayShootPosition, -rayDir);
             hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
             if (hitSomething)
             {
                 spawnerSet.Add(Instantiate(spawner));
-                spawnerSet[1].transform.position = hitInfo.point;
+                spawnerSet[1].transform.position = hitInfo.point;// + new Vector3(hitInfo.normal.x + floorOffset, hitInfo.normal.y, hitInfo.normal.z);
                 spawnerSet[1].transform.up = hitInfo.normal;
-                spawnerSet[1].transform.SetParent(GameObject.Find("ObjectHolder").transform);
+                spawnerSet[1].transform.SetParent(ground.transform.Find("ObjectHolder").transform);
             }
 
-            rayShootPosition -= 2 * transform.forward * sideOffsets;
+            rayShootPosition -= 2 * ground.transform.Find("Ground").transform.forward * sideOffsets;
             r = new Ray(rayShootPosition, -rayDir);
             hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
             if (hitSomething)
             {
                 spawnerSet.Add(Instantiate(spawner));
-                spawnerSet[2].transform.position = hitInfo.point;
+                spawnerSet[2].transform.position = hitInfo.point;// + new Vector3(hitInfo.normal.x + floorOffset, hitInfo.normal.y, hitInfo.normal.z);
                 spawnerSet[2].transform.up = hitInfo.normal;
-                spawnerSet[2].transform.SetParent(GameObject.Find("ObjectHolder").transform);
+                spawnerSet[2].transform.SetParent(ground.transform.Find("ObjectHolder").transform);
             }
 
             spawnerSetList.Add(spawnerSet);
         }
     }
 
-    void DeleteSpawners()
+    private void SpawnersSwitch()
     {
         foreach (List<GameObject> set in spawnerSetList)
         {
@@ -95,52 +105,58 @@ public class obstacleGenerator : MonoBehaviour
             {
                 if (set[i].transform.position.y < 0)
                 {
-                    //spawnerSetList.Remove
-                    Destroy(set[i]);
+                    //Deactivate Spawner
+                    set[i].GetComponent<spawner>().DeactivateSpawner();
                 }
+
+                if (set[i].transform.position.y >= 0)
+                {
+                    //Activate Spawner
+                    set[i].GetComponent<spawner>().ActivateSpawner();
+                }
+
+                set[i].GetComponent<spawner>().SpawnChance = spawnChance;
             }
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        for (float angle = 0; angle <= 360; angle += rowOffset)
+        {
+            Vector3 rayDir = Vector3.right;
+            Quaternion rot = Quaternion.AngleAxis(angle, ground.transform.Find("Ground").transform.forward);
+            rayDir = rot * rayDir;
 
+            Vector3 rayShootPosition = (ground.transform.Find("Ground").transform.position + (rayDir * RayDistance));
 
-    //private void OnDrawGizmos()
-    //{
-    //    for (float angle = 0; angle <= 360; angle += rowOffset)
-    //    {
-    //        Vector3 rayDir = Vector3.right;
-    //        Quaternion rot = Quaternion.AngleAxis(angle, transform.forward);
-    //        rayDir = rot * rayDir;
+            Ray r = new Ray(rayShootPosition, -rayDir);
+            RaycastHit hitInfo;
+            bool hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
+            if (hitSomething)
+            {
+                //Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
+                Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
+            }
 
-    //        Vector3 rayShootPosition = (transform.position + (rayDir * RayDistance));
+            rayShootPosition += ground.transform.Find("Ground").transform.forward * sideOffsets;
 
-    //        Ray r = new Ray(rayShootPosition, -rayDir);
-    //        RaycastHit hitInfo;
-    //        bool hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
-    //        if (hitSomething)
-    //        {
-    //            Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
-    //            Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
-    //        }
+            r = new Ray(rayShootPosition, -rayDir);
+            hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
+            if (hitSomething)
+            {
+                //Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
+                Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
+            }
 
-    //        rayShootPosition += transform.forward * sideOffsets;
-
-    //        r = new Ray(rayShootPosition, -rayDir);
-    //        hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
-    //        if (hitSomething)
-    //        {
-    //            Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
-    //            Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
-    //        }
-
-    //        rayShootPosition -= 2 * transform.forward * sideOffsets;
-    //        r = new Ray(rayShootPosition, -rayDir);
-    //        hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
-    //        if (hitSomething)
-    //        {
-    //            Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
-    //            Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
-    //        }
-    //    }
-    //}
+            rayShootPosition -= 2 * ground.transform.Find("Ground").transform.forward * sideOffsets;
+            r = new Ray(rayShootPosition, -rayDir);
+            hitSomething = Physics.Raycast(r, out hitInfo, RayDistance);
+            if (hitSomething)
+            {
+                //Gizmos.DrawCube(hitInfo.point, new Vector3(2, 2, 2));
+                Gizmos.DrawRay(hitInfo.point, hitInfo.normal * 3);
+            }
+        }
+    }
 }
---
+++ GJ3/Assets/Scripts/spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    private float spawnChance;
+    private bool isActivated = false;
+    public List<GameObject> obstacleList;
+
+    public float SpawnChance
+    {
+        get { return spawnChance; }
+        set { spawnChance = value; }
+    }
+    
+    public void ActivateSpawner()
+    {
+        if (isActivated == false)
+        {
+            float rand = Random.Range(0, 100);
+
+            if (rand <= spawnChance)
+            {
+                int objRand = Random.Range(0, obstacleList.Count);
+                GameObject childObj = Instantiate(obstacleList[objRand]);
+                childObj.transform.position = transform.position;
+                childObj.transform.up = transform.up;
+                childObj.transform.parent = transform;
+            }
+
+            isActivated = true;
+        }
+    }
+
+    public void DeactivateSpawner()
+    {
+        if (isActivated == true)
+        {
+            if (transform.childCount > 0)
+            {
+                for (int i = 0; i < transform.childCount; ++i)
+                {
+                    Destroy(transform.GetChild(i).gameObject);
+                }
+            }
+
+            isActivated = false;
+        }
+    }
+}
